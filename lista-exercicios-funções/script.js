97063a6138b5e6f0d4e44738d67e1d7aa604799d
Rafaelferrte <rafaelferrete333@gmail.com>
2026-08-12 11:15:23 -0300

    Lista de exercicios

--- lista-exercicios-funções/script.js
+++ lista-exercicios-funções/script.js
@@ -1,18 +1,17 @@
 //Função simples
-function mostrarDados(){
-    console.log(`Nome: Rafael`);
-    console.log(`Idade: 18`);
-    console.log(`Cidade: Registro`);
+function mostrarInformacoes(){
+    document.write(`Nome: Rafael<br>Idade: 18<br>Cidade: Registro<br>`);
+
 }
 
-mostrarDados();
+mostrarInformacoes();
 
 //Função com parâmetros 
 function dividir(num1,num2){
     return num1/num2;
 }
 
-console.log(dividir(4,2));
+document.write(`O resultado da divisão é ${dividir(4,2)}`);
 
 //Função com retorno
 function multi(a,b,c){
@@ -20,7 +19,7 @@ function multi(a,b,c){
     
 };
 
-console.log(multi(6,9,10));
+document.write(`<br>A multiplicação é ${multi(6,9,10)}`);
 
 //Função com mais de um retorno
 function maiorIdade(idade){
@@ -32,7 +31,7 @@ function maiorIdade(idade){
     }
 }
 
-console.log(maiorIdade(20));
+document.write(`<br>Você é ${maiorIdade(20)}`);
 
 // Função anonima
 const mencao = function (n1,n2) {
@@ -47,7 +46,7 @@ const mencao = function (n1,n2) {
 const n1 = 9;
 const n2 = 7;
 
-console.log(mencao(n1,n2));
+document.write(`<br>${mencao(n1,n2)}<br>`);
 
 //Arrow function com parâmetro único 
 
@@ -57,7 +56,7 @@ const triplo = (x) => {
 
 const x = 109;
 
-console.log(`O triplo do número é ${triplo(x)}`);
+document.write(`O triplo do número é ${triplo(x)}`);
 
 //Arrow function com mais de um parâmetro
 const soma = (a,b,c,d) => {
@@ -69,9 +68,9 @@ const b = 5;
 const c =68;
 const d = 84;
 
-console.log(`A soma é ${soma(a,b,c,d)}`);
+cdocument.write(`A soma é ${soma(a,b,c,d)}`);
 
 //IIFE
 const saudacao = (function (nome){
-    console.log(`Olá, ${nome}! Seja bem-vindo.`);
+    document.write(`Olá, ${nome}! Seja bem-vindo.`);
 })("Rafael");
